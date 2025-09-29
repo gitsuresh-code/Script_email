070c3ed72296978573de59f6fecc8f1d34d125a5
SureshU <sureshdevops@gmail.com>
2025-09-29 17:38:39 +0530

    message

---
+++ Startup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+user=$(id -u)
+
+if [ $user -ne 0 ];then
+    echo "Please take root permission"
+    exit 1
+fi
+
+
+sudo dnf install msmtp -y
+if [ $? -ne 0 ];then
+    echo "Installing msmtp on system"
+    else
+    echo "msmtp is already installed"
+fi
+
+cp /root/script_email/msmtprc /etc/
+
+chmod ugo+x /etc/msmtprc
+
+touch /var/log/msmtp.log
+chmod ugo+x /var/log/msmtp.log
+
+
+{
+echo "To: usuresh123456@gmail.com"
+echo "Subject: Test Email Alert"
+echo "Content-Type: text/html"
+echo ""
+echo "This is test email"
+} | msmtp "usuresh123456@gmail.com"
