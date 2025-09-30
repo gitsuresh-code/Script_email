9c3051faf1ed719de8df028351aa3767ccfd4ca5
SureshU <sureshdevops@gmail.com>
2025-09-30 18:21:28 +0530

    Message

--- memory_check.sh
+++ memory_check.sh
@@ -4,7 +4,7 @@ G="\e[32m"
 Y="\e[33m"
 
 DISK_USAGE=$(free -h | awk '/^Mem:/ {print $7}' | cut -d "M" -f1)
-DISK_THRESHOLD=100 # in project we keep it as 75
+DISK_THRESHOLD=500 # in project we keep it as 75
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
 AVAILABLE=$(free -h | awk '/^Mem:/ {print "Available:", $7}')
