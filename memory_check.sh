0db599133eda25c9324294c65af38e91814e250a
SureshU <sureshdevops@gmail.com>
2025-09-30 18:03:54 +0530

    Message

--- memory_check.sh
+++ memory_check.sh
@@ -7,13 +7,13 @@ DISK_USAGE=$(free -h | awk '/^Mem:/ {print $7}' | cut -d "M" -f1)
 DISK_THRESHOLD=100 # in project we keep it as 75
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
 MESSAGE=""
-
 AVAILABLE=$(free -h | awk '/^Mem:/ {print "Available:", $7}')
+
     if [ $DISK_USAGE -le $DISK_THRESHOLD ]; then
-        MESSAGE="Memory is low $R $AVAILABLE $N"
+        MESSAGE="Memory is low $R $AVAILABLE $N <br>"
         echo -e "$MESSAGE"
         else
-         MESSAGE="$G Memory looks good $MESSAGE $R $AVAILABLE $N"
+         MESSAGE="$G Memory looks good $G $AVAILABLE $N <br>"
          echo -e "$MESSAGE"
     fi
 
